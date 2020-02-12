f820b49a34a211a85edaa8d9540d04678cbc532f
ZackarieP <51347817+ZackarieP@users.noreply.github.com>
2020-02-12 13:41:40 -0500

    Fix name

--- parrotTroubleChallenge.java
+++ parrotTroubleChallenge.java
@@ -2,31 +2,31 @@
 public class parrotTroubleChallenge {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println(parrotTrouble(true, 6)); // true
 		System.out.println(parrotTrouble(true, 7)); // false
 		System.out.println(parrotTrouble(false, 6)); // false
-		
 
 	} // end main method
-	
-//	We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. 
-//	We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
-//
-//
-//	parrotTrouble(true, 6) → true
-//	parrotTrouble(true, 7) → false
-//	parrotTrouble(false, 6) → false
+
+	// We have a loud talking parrot. The "hour" parameter is the current hour time
+	// in the range 0..23.
+	// We are in trouble if the parrot is talking and the hour is before 7 or after
+	// 20. Return true if we are in trouble.
+	//
+	//
+	// parrotTrouble(true, 6) → true
+	// parrotTrouble(true, 7) → false
+	// parrotTrouble(false, 6) → false
 
 	public static boolean parrotTrouble(boolean talking, int hour) {
-		
+
 		if (talking && (hour < 7 || hour > 20)) {
 			return true;
 		} else {
 			return false;
 		}
-		  
-	} // end parrotTrouble method
 
+	} // end parrotTrouble method
 
 } // end class
